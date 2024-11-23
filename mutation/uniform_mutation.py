f240b37aabd0a26a425d67945a9e694be35b96be
Konrad <konradnrog@gmail.com>
2024-11-23 10:01:15 +0100

    Improved the uniform mutation method to ensure it stays within the range.

--- mutation/uniform_mutation.py
+++ mutation/uniform_mutation.py
@@ -7,13 +7,16 @@ class UniformMutation(MutationMethod):
         self.mutation_rate = mutation_rate
         self.gene_range = gene_range
 
+
     def mutate(self, chromosomes_to_mutate):
         mutated_chromosomes = []
         for chromosome in chromosomes_to_mutate:
             new_genes = []
             for gene in chromosome.genes:
                 if random.random() < self.mutation_rate:
-                    new_genes.append(random.uniform(*self.gene_range))
+                    new_gene = random.uniform(*self.gene_range)
+                    new_gene = max(min(new_gene, self.gene_range[1]), self.gene_range[0])  # Ensure gene is within range
+                    new_genes.append(new_gene)
                 else:
                     new_genes.append(gene)
             mutated_chromosomes.append(RealValuedChromosome(len(new_genes), genes=new_genes))
